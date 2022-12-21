f7ad1be46bf6c2d4f3f56f2ba0146e9007ceda18
guyeisenbach <guy.eisenbach@humansecurity.com>
2022-12-21 16:19:26 +0200

    remove unnecessary echo

--- babel-inplace.sh
+++ babel-inplace.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DIR=$1
-echo "$DIR"
 TEMPDIR=$(mktemp -d /tmp/babel-in-place.XXXXXXX)
 babel --copy-files --presets=@babel/preset-env,@babel/preset-react "$DIR" --out-dir "$TEMPDIR"
 rm -rf "$DIR"
