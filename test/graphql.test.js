f4b38fe0add395c0b27d735602efb82e7b1be2d5
guyeisenbach <guy.eisenbach@humansecurity.com>
2023-01-09 11:53:27 +0200

    moving the tests to other file

---
+++ test/graphql.test.js
@@ -0,0 +1,144 @@
+const pxutil = require('../lib/pxutil');
+const { assert } = require('sinon');
+const { isSensitiveGraphqlOperation } = require('../lib/pxutil');
+describe('Graphql Testing', () => {
+
+    it('should extract graphql data from the request body properly', () => {
+        const GRAPHQL_OPERATION_NAME = 'OperationName';
+        const GRAPHQL_OPERATION_TYPE = 'mutation';
+        const req = {
+            path: '/some/path/with/graphql',
+            body: {
+                query: `${GRAPHQL_OPERATION_TYPE} ${GRAPHQL_OPERATION_NAME} {\n    __typename\n}`,
+            },
+        };
+        const graphqlData = pxutil.getGraphqlData(req.body);
+        graphqlData.operationType.should.be.exactly(GRAPHQL_OPERATION_TYPE);
+        graphqlData.operationName.should.be.exactly(GRAPHQL_OPERATION_NAME);
+    });
+
+    it('extract with spaces', () => {
+        const gqlObj = {
+            query: '\n   query    q1 { \n abc \n }',
+            operationName: 'q1',
+        };
+
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        graphqlData.operationName.should.be.exactly('q1');
+        graphqlData.operationType.should.be.exactly('query');
+    });
+
+    it('extract with many queries', () => {
+        const gqlObj = {
+            query: 'query q1 { \n abc \n }\nmutation q2 {\n def\n }',
+            operationName: 'q2',
+        };
+
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        graphqlData.operationName.should.be.exactly('q2');
+        graphqlData.operationType.should.be.exactly('mutation');
+        assert.match(Object.keys(graphqlData.variables).length === 0, true);
+    });
+
+    it('extract with only one query without given operationName', () => {
+        const gqlObj = {
+            query: 'query q1 { \n abc \n }',
+        };
+
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        graphqlData.operationName.should.be.exactly('q1');
+        graphqlData.operationType.should.be.exactly('query');
+    });
+
+    it('should return null when multiple operations without explicitly specified', () => {
+        const gqlObj = {
+            query: 'query q1 { \n abc \n }\nmutation q2 {\n def\n }',
+        };
+
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        assert.match(graphqlData, null);
+    });
+
+    it('should include variables', () => {
+        const gqlObj = {
+            query: 'query q1(m: $x) { \n abc \n }\nmutation q2 {\n def\n }',
+            operationName: 'q1',
+            variables: { x: 2, y: 3, z: 4 },
+        };
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        graphqlData.operationName.should.be.exactly('q1');
+        graphqlData.operationType.should.be.exactly('query');
+        assert.match(graphqlData.variables.length === 3 &&
+            ['x', 'y', 'z'].every((e, i) => e === graphqlData.variables[i]), true);
+    });
+
+    it('sensitive information is not present in the graphql data parsing', () => {
+        const gqlObj = {
+            query: 'query q1(m: $email) { \n abc \n }\nmutation q2 {\n def\n }',
+            operationName: 'q1',
+            variables: { email: 'test@mail.com', password: 'Password1' },
+        };
+
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        assert.match(!!graphqlData.toString().match(/test@mail\.com/), false);
+        assert.match(!!graphqlData.toString().match(/Password1/), false);
+    });
+
+    it('nested values should extract keys recursively', () => {
+        const gqlObj = {
+            query: 'query q1(m: $email) { \n abc \n }\nmutation q2 {\n def\n }',
+            operationName: 'q1',
+            variables: {
+                email: 'test@mail.com', password: 'Password1', data: {
+                    key1: 'test',
+                    key2: {
+                        mostInnerKey: null,
+                    },
+                    key3: 10,
+                    emptyKey: {},
+                },
+            },
+        };
+
+        const graphqlData = pxutil.getGraphqlData(gqlObj);
+        assert.match(!!graphqlData.toString().match(/test@mail\.com/), false);
+        assert.match(!!graphqlData.toString().match(/Password1/), false);
+        assert.match(!!graphqlData.toString().match(/test/), false);
+        assert.match(!!graphqlData.toString().match(/10/), false);
+        assert.match(!!graphqlData.toString().match(/null/), false);
+
+        const expected = ['email', 'password', 'data.key1', 'data.key2.mostInnerKey', 'data.key3', 'data.emptyKey'];
+        assert.match(graphqlData.variables.length === expected.length &&
+            expected.every((e, i) => e === graphqlData.variables[i]), true);
+    });
+
+    it(`check for sensitive operation`, () => {
+        const gqlData = {
+            operationName: 'q1',
+            operationType: 'mutation',
+            variables: { x: 2 },
+        };
+        const config = {
+            SENSITIVE_GRAPHQL_OPERATION_TYPES: ['mutation'],
+            SENSITIVE_GRAPHQL_OPERATION_NAMES: ['q1'],
+        };
+
+        assert.match(isSensitiveGraphqlOperation(gqlData, config), true);
+        assert.match(isSensitiveGraphqlOperation(gqlData, {
+            ...config,
+            SENSITIVE_GRAPHQL_OPERATION_TYPES: ['query'],
+        }), true);
+        assert.match(isSensitiveGraphqlOperation({
+            ...gqlData,
+            operationName: 'q2',
+        }, config), true);
+
+        assert.match(isSensitiveGraphqlOperation({
+            ...gqlData,
+            operationName: 'q2',
+        }, {
+            ...config,
+            SENSITIVE_GRAPHQL_OPERATION_TYPES: ['query'],
+        }), false);
+    });
+});
