ce3596808cb7be59c80859d733838c827c538736
eldarlandman <eldarlandman@humansecurity.com>
2024-01-11 18:58:45 +0200

    - Fix unit-tests to support req.get

--- test/pxenforcer.test.js
+++ test/pxenforcer.test.js
@@ -40,6 +40,9 @@ describe('PX Enforcer - pxenforcer.js', () => {
         req.ip = '1.2.3.4';
         req.hostname = 'example.com';
         req.get = (key) => {
+            if (key === 'host') {
+                return req.host;
+            }
             return req.headers[key] || '';
         };
 
